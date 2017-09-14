e90cbaef9f8cda42ce2f8c3d4bbb6c402ef67a2b
Umar <ounu_aziz@yahoo.com>
2017-09-13 22:05:14 -0700

    Not edited this project, Will Edit Later

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        FridayShoppingCartEntities db = new FridayShoppingCartEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult Search(string word)
+        {
+            if (!string.IsNullOrEmpty(word))
+            {
+                word = word.ToLower();
+
+                var products = db.Products.Where(w => w.Name.ToLower().Contains(word));
+
+                return PartialView(products);
+            }
+            else
+            {
+                var products = db.Products.ToList();
+
+                return PartialView(products);
+            }
+             
+        }
+    }
+}
